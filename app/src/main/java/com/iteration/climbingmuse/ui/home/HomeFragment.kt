bfd00090d3383ec6b12c722553fe04960fdfb9de
Tchong-Kite Huam <tchong-kite.huam@thalesgroup.com>
2024-02-27 23:54:07 +0100

    Managing PreviewView- to display the camera

--- app/src/main/java/com/iteration/climbingmuse/ui/home/HomeFragment.kt
+++ app/src/main/java/com/iteration/climbingmuse/ui/home/HomeFragment.kt
@@ -1,21 +1,49 @@
 package com.iteration.climbingmuse.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.core.UseCase
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.iteration.climbingmuse.databinding.FragmentHomeBinding
+import com.iteration.climbingmuse.ui.OverlayView
+import timber.log.Timber
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class HomeFragment : Fragment() {
 
+    private lateinit var backgroundExecutor: ExecutorService
+    private var imageAnalyzer: ImageAnalysis? = null
+    private lateinit var preview: Preview
     private var _binding: FragmentHomeBinding? = null
+
+    private val REQUEST_CODE_CAMERA_PERMISSION = 101
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,15 +56,80 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val overlay: OverlayView = binding.overlay
+        val cameraFeed: PreviewView = binding.viewFinder
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Init the background executor
+        backgroundExecutor = Executors.newSingleThreadExecutor()
+
+        setUpCamera()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setUpCamera() {
+        val cameraProviderFuture =
+            ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(
+            {
+                // CameraProvider
+                cameraProvider = cameraProviderFuture.get()
+
+                // Build and bind the camera use cases
+                bindCameraUseCases()
+            }, ContextCompat.getMainExecutor(requireContext())
+        )
+    }
+
+    private fun bindCameraUseCases() {
+        // Preview. Only using the 4:3 ratio because this is the closest to our models
+        preview = Preview.Builder()
+            .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            //.setTargetRotation(binding.viewFinder.display.rotation) // FIXME Seems to crash here
+            .build()
+
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build() //TODO allow LENS_FACING_FRONT
+
+        // Attach the viewfinder's surface provider to preview use case
+        preview.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+
+
+        // CameraProvider
+        val cameraProvider = cameraProvider ?: throw IllegalStateException("Camera initialization failed.")
+        val useCases = arrayOf(preview) // TODO reintroduce imageAnalyzer (add imageAnalyzer to varargs)
+        camera = cameraProvider.bindToLifecycle(this, cameraSelector, *useCases)
+
+
+
+        /*
+        // ImageAnalysis. Using RGBA 8888 to match how our models work
+        imageAnalyzer =
+            ImageAnalysis.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(binding.viewFinder.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                // The analyzer can then be assigned to the instance
+                .also {
+                    it.setAnalyzer(backgroundExecutor) { image ->
+                        detectPose(image)
+                    }
+                }
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+        */
+
+        // A variable number of use-cases can be passed here -
+        // camera provides access to CameraControl & CameraInfo
     }
 }
