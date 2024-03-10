66ebc4fdc3e2650c2b8ca52762b77ee738ce1974
Tchong-Kite Huam <tchong.kite.h@gmail.com>
2024-03-10 17:45:01 +0100

    Adding a navigation rail to settings tab. Navigation rail will allow navigation between different settings options.

--- app/src/main/java/com/iteration/climbingmuse/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/iteration/climbingmuse/ui/settings/SettingsFragment.kt
@@ -6,7 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigationrail.NavigationRailView
+import com.iteration.climbingmuse.R
 import com.iteration.climbingmuse.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -16,6 +23,7 @@ class SettingsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,8 +35,13 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        vm.text.observe(viewLifecycleOwner) { textView.text = it }
+        val navRail : NavigationRailView = binding.settingsNavigationRail
+        val frag : FragmentContainerView = binding.settingsFragment
+
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.settings_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        NavigationUI.setupWithNavController(navRail, navController)
+
         return root
     }
 
