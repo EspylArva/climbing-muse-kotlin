0bfeba5d0916dbc5cc090bc46cf79d5acd2cf8a1
Tchong-Kite Huam <tchong.kite.h@gmail.com>
2024-03-11 00:20:29 +0100

    Managing subcheckboxes behaviour.

--- app/src/main/java/com/iteration/climbingmuse/ui/MaterialViewHelper.kt
+++ app/src/main/java/com/iteration/climbingmuse/ui/MaterialViewHelper.kt
@@ -1,4 +1,57 @@
 package com.iteration.climbingmuse.ui
 
+import android.widget.CheckBox
+import androidx.appcompat.widget.AppCompatCheckBox
+import com.google.android.material.checkbox.MaterialCheckBox
+
 class MaterialViewHelper {
+
+    companion object {
+
+        fun MaterialCheckBox.setSubCheckboxes(childrenCheckBoxes: Sequence<AppCompatCheckBox>) {
+            var isUpdatingChildren = false
+
+            val parentOnCheckedStateChangedListener =
+                MaterialCheckBox.OnCheckedStateChangedListener { checkBox: MaterialCheckBox, state: Int ->
+                    val isChecked = checkBox.isChecked
+                    if (state != MaterialCheckBox.STATE_INDETERMINATE) {
+                        isUpdatingChildren = true
+                        for (child in childrenCheckBoxes) {
+                            child.isChecked = isChecked
+                        }
+                        isUpdatingChildren = false
+                    }
+                }
+            this.addOnCheckedStateChangedListener(parentOnCheckedStateChangedListener)
+
+            // Checked state changed listener for each child
+            val childOnCheckedStateChangedListener =
+                MaterialCheckBox.OnCheckedStateChangedListener { checkBox: MaterialCheckBox?, state: Int ->
+                    if (!isUpdatingChildren) {
+                        this.setParentState(childrenCheckBoxes, parentOnCheckedStateChangedListener)
+                    }
+                }
+            for (child in childrenCheckBoxes) {
+                (child as MaterialCheckBox).addOnCheckedStateChangedListener(childOnCheckedStateChangedListener)
+            }
+        }
+
+        private fun MaterialCheckBox.setParentState(childrenCheckBoxes: Sequence<CheckBox>, parentOnCheckedStateChangedListener: MaterialCheckBox.OnCheckedStateChangedListener) {
+            val checkedCount = childrenCheckBoxes.filter { obj: CheckBox -> obj.isChecked }
+                .count()
+            val allChecked = checkedCount == childrenCheckBoxes.count()
+            val noneChecked = checkedCount == 0
+
+            this.removeOnCheckedStateChangedListener(parentOnCheckedStateChangedListener)
+            if (allChecked) {
+                this.isChecked = true
+            } else if (noneChecked) {
+                this.isChecked = false
+            } else {
+                this.checkedState = MaterialCheckBox.STATE_INDETERMINATE
+            }
+            this.addOnCheckedStateChangedListener(parentOnCheckedStateChangedListener)
+        }
+    }
+
 }
