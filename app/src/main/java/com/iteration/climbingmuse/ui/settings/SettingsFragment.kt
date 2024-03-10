e9880f042eec064fce2a63f91e6c11f1087b8a14
Tchong-Kite Huam <tchong.kite.h@gmail.com>
2024-03-10 18:59:27 +0100

    Fixing minor GUI issues, and preparing potential badge implementation on Navigation Rail.

--- app/src/main/java/com/iteration/climbingmuse/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/iteration/climbingmuse/ui/settings/SettingsFragment.kt
@@ -1,10 +1,10 @@
 package com.iteration.climbingmuse.ui.settings
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
@@ -12,9 +12,14 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.internal.BaselineLayout
+import com.google.android.material.navigation.NavigationBarItemView
 import com.google.android.material.navigationrail.NavigationRailView
+import com.google.android.material.textview.MaterialTextView
 import com.iteration.climbingmuse.R
 import com.iteration.climbingmuse.databinding.FragmentSettingsBinding
+
 
 class SettingsFragment : Fragment() {
 
@@ -42,7 +47,42 @@ class SettingsFragment : Fragment() {
         navController = navHostFragment.findNavController()
         NavigationUI.setupWithNavController(navRail, navController)
 
+        setupNavigationRailText(navRail)
+        setBadges(navRail)
+
+
         return root
+    }
+
+    private fun setBadges(navRail: NavigationRailView) {
+        // FIXME: This is just an example
+        navRail.getOrCreateBadge(R.id.navigation_computer_vision_settings).apply {
+            isVisible = true
+            number = 2
+            badgeGravity = BadgeDrawable.TOP_END
+        }
+    }
+
+    private fun setupNavigationRailText(navRail: NavigationRailView) {
+        navRail.findViewById<NavigationBarItemView>(R.id.navigation_computer_vision_settings).apply {
+            setItemMaxLines(
+                this.findViewById<BaselineLayout>(com.google.android.material.R.id.navigation_bar_item_labels_group),
+                2
+            )
+        }
+
+    }
+
+    private fun setItemMaxLines(baselineLayout: BaselineLayout?, maxLines: Int) {
+        if (baselineLayout != null) {
+            for (i in 0 until baselineLayout.childCount) {
+                val child = baselineLayout.getChildAt(i)
+                if (child is MaterialTextView) {
+                    child.maxLines = maxLines
+                    child.gravity = Gravity.CENTER
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
